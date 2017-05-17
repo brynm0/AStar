614df7435766aa4cdb2fe72f5d8815d599e967ed
Bryn Murrell <brynmurrell97@gmail.com>
2017-05-17 18:39:07 +1000

    Initial commit

---
+++ AStar/AStarInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace AStar
+{
+    public class AStarInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "AStar";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("4f2e0526-de73-4ccd-b428-fbd852d5ac8f");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
---
+++ AStar/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Rhino.Geometry;
+
+
+namespace AStar
+{
+    public class Node
+    {
+        public Vector3f position { get; set; }
+        public List<int> connections { get; set; }
+        public Node (Vector3f _position, List<int> _connections)
+        {
+            this.position = _position;
+            this.connections = _connections;
+        }
+    }
+}
